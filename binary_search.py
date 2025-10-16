e849483de91cea42036632c1f836bb076c72b9d4
fatemearyaie <fatemearyaie9@gmail.com>
2025-10-16 11:23:18 +0330

    add binary_search file

---
+++ binary_search.py
@@ -0,0 +1,16 @@
+def binarySearch(arr,target):
+    left,right = 0, len(arr) - 1
+    while left<=right:
+        mid = (left+right)//2
+        if mid == target:
+            return mid
+        if mid < target:
+            right = mid + 1
+        else:
+            left = mid - 1
+
+
+
+nums = [1,2,3,4,5,6,7,8,9,10,12,14,16,18,20,22,24,26,28,30]
+result = binarySearch(nums, 18)
+print(result)
