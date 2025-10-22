0588f6f89a633bd4e2a6da5053ee0e1cd74bdaa7
fatemearyaie <fatemearyaie9@gmail.com>
2025-10-22 08:57:43 +0330

    add o notion as comment

--- selection_sort.py
+++ selection_sort.py
@@ -1,3 +1,4 @@
+# O(n^2)
 def selectionSort(arr):
     n = len(arr)
     for i in range(n):
@@ -5,9 +6,4 @@ def selectionSort(arr):
         for j in range(i+1, n):
             if arr[j] < arr[min_index]:
                 min_index = j
-        arr[i], arr[min_index] = arr[min_index], arr[i]
-    return arr
-
-nums = [0,5,15,45,7,45,23,6,0,56,100,99,9,63,25,75,22,19,80]
-result = selectionSort(nums)
-print(result)
+        arr[i], arr[min_index]= arr[min_index], arr[i]
